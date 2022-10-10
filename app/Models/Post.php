5c3c7e75c8bc25c0781ca29d12519f760a00325d
simonebonifazi <simonbonifraz@gmail.com>
2022-10-10 07:52:47 +0200

    fixed fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,5 +19,5 @@ class Post extends Model
         return $this->belongsToMany('App\Models\Tag');
     }
 
-    protected $fillable = ['title', 'image', 'content','category_id'];
+    protected $fillable = ['title', 'content','category_id'];
 }
--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -28,7 +28,7 @@ class PostSeeder extends Seeder
             $new_post->category_id = Arr::random($category_ids);
             $new_post->is_published = $faker->boolean();
             $new_post->content = $faker->paragraphs(2, true);
-            $new_post->image = $faker->imageUrl(150, 150);
+            //$new_post->image = $faker->imageUrl(150, 150);
             $new_post->slug = Str::slug($new_post->title, '-');
 
             $new_post->save();
